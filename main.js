5fbb265f72548e37cab3ca0d7b0099b7ad83e600
maxGrachev <mxmgrachev@gmail.com>
2018-04-05 10:22:52 +0300

    ball with control from keyboard

--- main.js
+++ main.js
@@ -1,10 +1,7 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext("2d");
-
 var width = canvas.width;
 var height = canvas.height;
-var ballsNumber =200;
-var maxSpeed = 5;
 
 function circle(x, y, radius, fillCircle, color) {
   ctx.beginPath();
@@ -20,11 +17,11 @@ function circle(x, y, radius, fillCircle, color) {
 }
 
 var Ball = function () {
-  this.radius = 5;
+  this.radius = 10;
   this.x = Math.random() * (width - 2 * this.radius);
   this.y = Math.random() * (height - 2 * this.radius);
-  this.xSpeed = Math.random() * 2 * maxSpeed - maxSpeed;
-  this.ySpeed = Math.random() * 2 * maxSpeed - maxSpeed;
+  this.xSpeed = 5;
+  this.ySpeed = 0;
   this.color = 'grey'
 };
 
@@ -32,35 +29,62 @@ Ball.prototype.draw = function () {
   circle(this.x, this.y, this.radius, true, this.color);
 }
 
+
 Ball.prototype.move = function () {
   this.x = this.x + this.xSpeed;
   this.y = this.y + this.ySpeed;
-}
 
-Ball.prototype.checkCollision = function () {
-  if (this.x - this.radius < 0 || this.x + this.radius > width) {
-    this.xSpeed = -this.xSpeed;
+  if (this.x < 0) {
+    this.x = width;
+  } else if (this.x > width) {
+    this.x = 0;
   }
-  if (this.y - this.radius < 0 || this.y + this.radius > height) {
-    this.ySpeed = -this.ySpeed;
+  if (this.y < 0) {
+    this.y = height;
+  } else if (this.y > height) {
+    this.y = 0;
   }
 }
 
-var ball = [];
-
-for (let i = 1; i <= ballsNumber; i++) {
-  ball[i] = new Ball();
+Ball.prototype.setDirection = function (direction) {
+  if (direction === "up") {
+    this.xSpeed = 0;
+    this.ySpeed = -5;
+  } else if (direction === "down") {
+    this.xSpeed = 0;
+    this.ySpeed = 5;
+  } else if (direction === "left") {
+    this.xSpeed = -5;
+    this.ySpeed = 0;
+  } else if (direction === "right") {
+    this.xSpeed = 5;
+    this.ySpeed = 0;
+  } else if (direction === "space") {
+    this.xSpeed = 0;
+    this.ySpeed = 0;
+  }
 }
+
+var ball = new Ball;
+
+var keyNames = {
+  32: "space",
+  37: "left",
+  38: "up",
+  39: "right",
+  40: "down"
+};
+
+$("body").keydown(function (event) {
+  var direction = keyNames[event.keyCode];
+  ball.setDirection(direction);
+});
 
 setInterval(function () {
   ctx.clearRect(0, 0, width, height);
 
-  for (let i = 1; i <= ballsNumber; i++) {
-    ball[i].draw();
-    ball[i].move();
-    ball[i].checkCollision();
-  }
-
+  ball.draw();
+  ball.move();
 
   ctx.strokeRect(0, 0, width, height);
 }, 30)
