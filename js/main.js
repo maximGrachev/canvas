b6e1d7ad1237bd2c6d2e08dda0de0ed28f4e5b64
maxGrachev <mxmgrachev@gmail.com>
2018-04-06 10:35:28 +0300

    p260

--- js/main.js
+++ js/main.js
@@ -2,6 +2,10 @@ var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext("2d");
 var width = canvas.width;
 var height = canvas.height;
+var blockSize = 10;
+var widthInBlocks = width / blockSize;
+var heightInBlocks = height / blockSize;
+var score = 0;
 
 function circle(x, y, radius, fillCircle, color) {
   ctx.beginPath();
@@ -16,56 +20,101 @@ function circle(x, y, radius, fillCircle, color) {
   }
 }
 
-var Ball = function () {
-  this.radius = 10;
-  this.x = Math.random() * (width - 2 * this.radius);
-  this.y = Math.random() * (height - 2 * this.radius);
-  this.xSpeed = 5;
-  this.ySpeed = 0;
-  this.color = 'grey'
-};
-
-Ball.prototype.draw = function () {
-  circle(this.x, this.y, this.radius, true, this.color);
+function drawBorder() {
+  ctx.fillStyle = "gray";
+  ctx.fillRect(0, 0, width, blockSize);
+  ctx.fillRect(width - blockSize, 0, blockSize, height);
+  ctx.fillRect(0, height - blockSize, width, blockSize);
+  ctx.fillRect(0, 0, blockSize, height);
 }
 
+function drawScore() {
+  ctx.font = "20px Arial";
+  ctx.fillStyle = "black";
+  ctx.textBaseline = "top";
+  ctx.textAlign = "left";
+  ctx.fillText("Score: " + score, blockSize, blockSize);
+}
 
-Ball.prototype.move = function () {
-  this.x = this.x + this.xSpeed;
-  this.y = this.y + this.ySpeed;
+function gameOver() {
+  clearInterval(intervalId);
+  ctx.font = "60px Arial";
+  ctx.fillStyle = "Black";
+  ctx.textAlign = "center";
+  ctx.textBaseline = "middle";
+  ctx.fillText("Game over", width / 2, height / 2);
+}
 
-  if (this.x < 0) {
-    this.x = width;
-  } else if (this.x > width) {
-    this.x = 0;
-  }
-  if (this.y < 0) {
-    this.y = height;
-  } else if (this.y > height) {
-    this.y = 0;
+var Block = function (col, row) {
+  this.col = col;
+  this.row = row;
+}
+
+Block.prototype.drawSquare = function (color) {
+  var x = this.col * blockSize;
+  var y = this.row * blockSize;
+
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, blockSize, blockSize);
+}
+
+Block.prototype.drawCircle = function (color) {
+  var centerX = this.col * blockSize + blockSize / 2;
+  var centerY = this.row * blockSize + blockSize / 2;
+
+  circle(centerX, centerY, blockSize / 2, true, color);
+}
+
+Block.prototype.equal = function (otherBlock) {
+  return this.col === otherBlock.col && this.row === otherBlock.row;
+}
+
+var Snake = function () {
+  this.segments = [
+    new Block(7, 5),
+    new Block(6, 5),
+    new Block(5, 5)
+  ];
+  this.direction = "right";
+  this.nextDirection = "right";
+}
+
+Snake.prototype.drawSnake = function () {
+  for (var i = 0; i < this.segments.length; i++) {
+    this.segments[i].drawSquare('black');
   }
 }
 
-Ball.prototype.setDirection = function (direction) {
-  if (direction === "up") {
-    this.xSpeed = 0;
-    this.ySpeed = -5;
-  } else if (direction === "down") {
-    this.xSpeed = 0;
-    this.ySpeed = 5;
-  } else if (direction === "left") {
-    this.xSpeed = -5;
-    this.ySpeed = 0;
-  } else if (direction === "right") {
-    this.xSpeed = 5;
-    this.ySpeed = 0;
-  } else if (direction === "space") {
-    this.xSpeed = 0;
-    this.ySpeed = 0;
+Snake.prototype.move = function () {
+  var head = this.segment[0];
+  var newHead;
+
+  this.direction = this.nextDirection;
+
+  if (this.direction === "right") {
+    newHead = new Block(head.col + 1, head.row);
+  } else if (this.direction === "down") {
+    newHead = new Block(head.col, head.row + 1);
+  } else if (this.direction === "left") {
+    newHead = new Block(head.col - 1, head.row);
+  } else if (this.direction === "up") {
+    newHead = new Block(head.col, head.row - 1);
+  }
+
+  if (this.checkCollision(newHead)) {
+    gameOver();
+    return;
+  }
+
+  this.segments.unshift(newHead);
+
+  if (newHead.equal(apple.position)) {
+    score++;
+    apple.move();
+  } else {
+    this.segment.pop();
   }
 }
-
-var ball = new Ball;
 
 var keyNames = {
   32: "space",
@@ -80,11 +129,17 @@ $("body").keydown(function (event) {
   ball.setDirection(direction);
 });
 
-setInterval(function () {
+
+
+var intervalId = setInterval(function () {
   ctx.clearRect(0, 0, width, height);
 
-  ball.draw();
-  ball.move();
+  drawScore();
+  /*snake.move();
+  snake.drow();
+  apple.drow();*/
+  drawBorder();
+  var sampleBlock = new Block(3, 4);
+  sampleBlock.drawCircle("LightBlue");
 
-  ctx.strokeRect(0, 0, width, height);
-}, 30)
+}, 100)
