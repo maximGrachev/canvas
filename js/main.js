3b3f1533b9b463e58208e8e31734e66ae6667547
Maxim Grachev <mxmgrachev@gmail.com>
2018-04-18 10:20:04 +0300

    created method of figute moving

--- js/main.js
+++ js/main.js
@@ -6,7 +6,8 @@ var blockSize = 20;
 var widthInBlocks = width / blockSize;
 var heightInBlocks = height / blockSize;
 var score = 0;
-var direction = {
+var allFigures = [];
+var action = {
   37: "left",
   38: "up",
   39: "right",
@@ -210,40 +211,210 @@ var Figure = function (type, x, y, rotation) {
     this.segments = [
       new Block(this.x, this.y),
       new Block(this.x - 1, this.y),
-      new Block(this.x-1, this.y + 1),
+      new Block(this.x - 1, this.y + 1),
+      new Block(this.x, this.y - 1)
+    ]
+  }
+
+}
+
+Figure.prototype.moveFigure = function (direction) {
+  
+  if (direction === "right"){
+    this.x = this.x + 1;
+  }
+  else if (direction === "left"){
+    this.x = this.x - 1;
+  }
+  else if (direction === "down"){
+    this.y = this.y + 2;
+  }
+  else if (direction === "up"){
+   if (this.rotation < 3){
+     this.rotation++;
+   }
+   else{
+    this.rotation = 0;
+   }
+  }
+  
+  if (this.type === 'I' && (this.rotation === 0 || this.rotation === 2)) {
+    this.segments = [
+      new Block(this.x, this.y),
+      new Block(this.x + 1, this.y),
+      new Block(this.x + 2, this.y),
+      new Block(this.x + 3, this.y)
+    ]
+  }
+  else if (this.type === 'I' && (this.rotation === 3 || this.rotation === 1)) {
+    this.segments = [
+      new Block(this.x, this.y),
+      new Block(this.x, this.y + 1),
+      new Block(this.x, this.y + 2),
+      new Block(this.x, this.y + 3)
+    ]
+  }
+  else if (this.type === 'J' && this.rotation === 0) {
+    this.segments = [
+      new Block(this.x, this.y),
+      new Block(this.x, this.y - 1),
+      new Block(this.x + 1, this.y),
+      new Block(this.x + 2, this.y)
+    ]
+  }
+  else if (this.type === 'J' && this.rotation === 1) {
+    this.segments = [
+      new Block(this.x, this.y),
+      new Block(this.x + 1, this.y),
+      new Block(this.x, this.y + 1),
+      new Block(this.x, this.y + 2)
+    ]
+  }
+  else if (this.type === 'J' && this.rotation === 2) {
+    this.segments = [
+      new Block(this.x, this.y),
+      new Block(this.x - 2, this.y),
+      new Block(this.x - 1, this.y),
+      new Block(this.x, this.y + 1)
+    ]
+  }
+  else if (this.type === 'J' && this.rotation === 3) {
+    this.segments = [
+      new Block(this.x, this.y),
+      new Block(this.x - 1, this.y),
+      new Block(this.x, this.y - 1),
+      new Block(this.x, this.y - 2)
+    ]
+  }
+  else if (this.type === 'L' && this.rotation === 0) {
+    this.segments = [
+      new Block(this.x, this.y),
+      new Block(this.x - 2, this.y),
+      new Block(this.x - 1, this.y),
+      new Block(this.x, this.y - 1)
+    ]
+  }
+  else if (this.type === 'L' && this.rotation === 1) {
+    this.segments = [
+      new Block(this.x, this.y),
+      new Block(this.x, this.y - 1),
+      new Block(this.x, this.y - 2),
+      new Block(this.x + 1, this.y)
+    ]
+  }
+  else if (this.type === 'L' && this.rotation === 2) {
+    this.segments = [
+      new Block(this.x, this.y),
+      new Block(this.x + 1, this.y),
+      new Block(this.x + 2, this.y),
+      new Block(this.x, this.y + 1)
+    ]
+  }
+  else if (this.type === 'L' && this.rotation === 3) {
+    this.segments = [
+      new Block(this.x, this.y),
+      new Block(this.x - 1, this.y),
+      new Block(this.x, this.y + 1),
+      new Block(this.x, this.y + 2)
+    ]
+  }
+  else if (this.type === 'O' && (this.rotation === 0 || this.rotation === 1 || this.rotation === 2 || this.rotation === 3)) {
+    this.segments = [
+      new Block(this.x, this.y),
+      new Block(this.x + 1, this.y),
+      new Block(this.x, this.y + 1),
+      new Block(this.x + 1, this.y + 1)
+    ]
+  }
+  else if (this.type === 'S' && (this.rotation === 0 || this.rotation === 2)) {
+    this.segments = [
+      new Block(this.x, this.y),
+      new Block(this.x + 1, this.y),
+      new Block(this.x, this.y + 1),
+      new Block(this.x - 1, this.y + 1)
+    ]
+  }
+  else if (this.type === 'S' && (this.rotation === 1 || this.rotation === 3)) {
+    this.segments = [
+      new Block(this.x, this.y),
+      new Block(this.x - 1, this.y),
+      new Block(this.x - 1, this.y - 1),
+      new Block(this.x, this.y + 1)
+    ]
+  }
+  else if (this.type === 'T' && this.rotation === 0) {
+    this.segments = [
+      new Block(this.x, this.y),
+      new Block(this.x - 1, this.y),
+      new Block(this.x + 1, this.y),
+      new Block(this.x, this.y - 1)
+    ]
+  }
+  else if (this.type === 'T' && this.rotation === 1) {
+    this.segments = [
+      new Block(this.x, this.y),
+      new Block(this.x + 1, this.y),
+      new Block(this.x, this.y - 1),
+      new Block(this.x, this.y + 1)
+    ]
+  }
+  else if (this.type === 'T' && this.rotation === 2) {
+    this.segments = [
+      new Block(this.x, this.y),
+      new Block(this.x + 1, this.y),
+      new Block(this.x - 1, this.y),
+      new Block(this.x, this.y + 1)
+    ]
+  }
+  else if (this.type === 'T' && this.rotation === 3) {
+    this.segments = [
+      new Block(this.x, this.y),
+      new Block(this.x - 1, this.y),
+      new Block(this.x, this.y - 1),
+      new Block(this.x, this.y + 1)
+    ]
+  }
+  else if (this.type === 'Z' && (this.rotation === 0 || this.rotation === 2)) {
+    this.segments = [
+      new Block(this.x, this.y),
+      new Block(this.x - 1, this.y),
+      new Block(this.x, this.y + 1),
+      new Block(this.x + 1, this.y + 1)
+    ]
+  }
+  else if (this.type === 'Z' && (this.rotation === 1 || this.rotation === 3)) {
+    this.segments = [
+      new Block(this.x, this.y),
+      new Block(this.x - 1, this.y),
+      new Block(this.x - 1, this.y + 1),
       new Block(this.x, this.y - 1)
     ]
   }
 }
 
-Figure.prototype.drawFigure = function () {
-  for (let i = 0; i < this.segments.length; i++) {
-    this.segments[i].drawSquare('gray');
-  }
-}
-
-var fig = new Figure('T', 3, 3, 0);
-
-$("body").keydown(function (event) {
-  let key = direction[event.keyCode];
- 
-
-  if (key !== undefined) {
-    if (key === 'down') {
-      console.log('click');
-      fig.y = fig.y + 1;
+  Figure.prototype.drawFigure = function () {
+    for (let i = 0; i < this.segments.length; i++) {
+      this.segments[i].drawSquare('gray');
     }
   }
-});
 
+  allFigures[0] = new Figure('T', 5, 2, 1);
 
+  $("body").keydown(function (event) {
+    let newAction = action[event.keyCode];
 
-var intervalId = setInterval(function () {
-  ctx.clearRect(0, 0, width, height);
+    if (newAction !== undefined) {
+      allFigures[0].moveFigure(newAction);
+    }
 
+  });
 
-  fig.drawFigure();
-  drawScore();
-  drawBorder();
+  var intervalId = setInterval(function () {
+    ctx.clearRect(0, 0, width, height);
 
-}, 100)
+    allFigures[0].drawFigure();
+    allFigures[0].moveFigure('down');
+    drawScore();
+    drawBorder();
+
+  }, 300)
